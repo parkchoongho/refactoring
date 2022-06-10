c53317302d655f3aff0070167672b905cf485f5c
parkchoongho <pchyo92@gmail.com>
2022-06-10 11:07:07 +0900

    slice sentence

--- ch1/1-2/index.js
+++ ch1/1-2/index.js
@@ -1,6 +1,4 @@
 function statement(invoice, plays) {
-  let totalAmount = 0;
-
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
@@ -8,6 +6,7 @@ function statement(invoice, plays) {
     result += `${playFor(perf).name}: ${usd(doAmountFor(perf))} (${perf.audience}석)\n`;
   }
 
+  let totalAmount = 0;
   for (let perf of invoice.performances) {
     totalAmount += doAmountFor(perf);
   }
