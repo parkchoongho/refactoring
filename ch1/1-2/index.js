588d2abc5ce656dabde48c608d1c796d8ad5b268
parkchoongho <pchyo92@gmail.com>
2022-06-19 16:42:09 +0900

    move customer data

--- ch1/1-2/index.js
+++ ch1/1-2/index.js
@@ -1,8 +1,10 @@
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
