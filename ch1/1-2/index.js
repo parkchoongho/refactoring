8e7a47864dbceaa591f3cf83f196da2dc388565c
parkchoongho <pchyo92@gmail.com>
2022-06-19 17:50:11 +0900

    move calculate fuction

--- ch1/1-2/index.js
+++ ch1/1-2/index.js
@@ -1,25 +1,26 @@
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
-}
 
-function renderPlainText(data, plays) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-  for (let perf of data.performances) {
-    // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${usd(doAmountFor(perf))} (${perf.audience}석)\n`;
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
+    result.amount = doAmountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
+    return result;
   }
 
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 
   function doAmountFor(aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case 'tragedy': // 비극
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -35,22 +36,47 @@ function renderPlainText(data, plays) {
         break;
 
       default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
     }
 
     return result;
   }
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
     return result;
   }
+
+  function totalVolumeCredits(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.volumeCredits;
+    }
+    return result;
+  }
+
+  function totalAmount(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.amount;
+    }
+    return result;
+  }
+}
+
+function renderPlainText(data, plays) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+  for (let perf of data.performances) {
+    // 청구 내역을 출력한다.
+    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+  return result;
 
   function usd(aNumber) {
     return new Intl.NumberFormat('en-US', {
@@ -58,22 +84,6 @@ function renderPlainText(data, plays) {
       currency: 'USD',
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
-  }
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
-    }
-    return result;
-  }
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += doAmountFor(perf);
-    }
-    return result;
   }
 }
 
